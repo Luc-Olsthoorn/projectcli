229b35d4ad08a1d49a4252c5ee74ed397072f721
Luc-Olsthoorn <lucols97@gmail.com>
2018-03-27 23:06:49 -0400

    got inital command from JSON working

--- skele/cli.py
+++ skele/cli.py
@@ -1,42 +1,23 @@
-"""
-skele
-
-Usage:
-  skele hello
-  skele -h | --help
-  skele --version
-
-Options:
-  -h --help                         Show this screen.
-  --version                         Show version.
-
-Examples:
-  skele hello
-
-Help:
-  For help using this tool, please open an issue on the Github repository:
-  https://github.com/rdegges/skele-cli
-"""
-
-
-from inspect import getmembers, isclass
-
-from docopt import docopt
-
+import sys
 from . import __version__ as VERSION
+import json
+from pprint import pprint
+from json import dumps
+from subprocess import call
+
+commandsList =["/Users/lucolsthoorn/Programming/cliproject/skele-cli/skele/commands/userCommand.json"]
+
+
 
 
 def main():
-    """Main CLI entrypoint."""
-    import skele.commands
-    options = docopt(__doc__, version=VERSION)
-
-    # Here we'll try to dynamically match the command the user is trying to run
-    # with a pre-defined command class we've already created.
-    for (k, v) in options.items(): 
-        if hasattr(skele.commands, k) and v:
-            module = getattr(skele.commands, k)
-            skele.commands = getmembers(module, isclass)
-            command = [command[1] for command in skele.commands if command[0] != 'Base'][0]
-            command = command(options)
-            command.run()
+  print(sys.argv)
+  for path in commandsList:
+      data = json.load(open(path))
+      print (sys.argv[1])
+      print (list(data)[0])
+      name = list(data)[0]
+      if sys.argv[1] == name:
+        subCommandsNames = list(data[name])
+        call(data[name][subCommandsNames[0]])
+    #call([)
